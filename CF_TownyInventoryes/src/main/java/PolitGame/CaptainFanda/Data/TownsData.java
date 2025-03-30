7ef5bd07039211cb2fb136984fea121ce8cba4fc
Капитан Фанда <captainFandik@gmail.com>
2025-03-30 16:15:25 +0300

    Add files via upload

---
+++ CF_TownyInventoryes/src/main/java/PolitGame/CaptainFanda/Data/TownsData.java
@@ -0,0 +1,33 @@
+package PolitGame.CaptainFanda.Data;
+
+import com.palmergames.bukkit.towny.object.Town;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TownsData {
+    private static File townFile;
+    private static String path = "plugins/Fox/TownyInvs/towns/";
+
+    public static void addTown(Town town) {
+        townFile = new File(path + town.getName() + ".yml");
+        YamlConfiguration ymlPut = YamlConfiguration.loadConfiguration(townFile);
+        try {
+            ymlPut.addDefault("items", "none");
+            ymlPut.options().copyDefaults(true);
+            ymlPut.save(townFile);
+        } catch (IOException var1) {
+            var1.fillInStackTrace();
+        }
+    }
+
+    public static YamlConfiguration getTownYAML(Town town) {
+        townFile = new File(path + town.getName() + ".yml");
+        return YamlConfiguration.loadConfiguration(townFile);
+    }
+    public static File getTownFile(Town town) {
+        townFile = new File(path + town.getName() + ".yml");
+        return townFile;
+    }
+}
